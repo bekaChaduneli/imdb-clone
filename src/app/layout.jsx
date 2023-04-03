1766e701f7e4428ebc64a2f0cb37e6a42aee9c70
beka chaduneli <beka@kali.ge>
2023-04-03 17:21:53 +0400

    add dark mode

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import "./globals.css";
+import Providers from "./Providers";
 
 export const metadata = {
   title: "IMDb Clone",
@@ -10,10 +11,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <>
-          <Header />
-          {children}
-        </>
+        <Providers>
+          <>
+            <Header />
+            {children}
+          </>
+        </Providers>
       </body>
     </html>
   );
